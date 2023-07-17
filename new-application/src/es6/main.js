8466d334cb8d9149c47e96c073ab1d5937e53f79
Dheepaganesh Prithiviraj <dheepaganeshprithiviraj@Dheepaganeshs-MacBook-Pro.local>
2023-07-17 17:46:30 +0530

    React new folder

--- new-application/src/es6/main.js
+++ new-application/src/es6/main.js
@@ -1,3 +1,5 @@
+
+
 // let name_ = 'Deepak';
 // console.log(name_)
 
@@ -37,4 +39,93 @@
 // console.log(ray)
 
 
+
+// const sayName = a =>{
+//   console.log('Hello',a)
+// }
+
+// sayName('Deepak')
+
+
+// const sayHello = function(){        //arguments work in normal functions
+//   console.log(arguments)
+// }
+
+// sayHello(1,'deepak',2001)
+
+
+// const sayHello1 =()=>{        //arguments not work in normal functions
+//   console.log(arguments)
+// }
+
+// sayHello1(1,'deepak',2001)
+
+// setTimeout(function(){
+//   console.log('Hello')
+// },5000)
+
+
+// In Arrow Function 
+
+// setTimeout(()=>{
+//     console.log('Hello')
+//   },5000)
+  
+
+// function sayHello(){
+//   console.log('After')
+// }
+
+// sayHello()=>{
+//     console.log('After')  arrow function doesn't have name bcoz it is anonymous function
+//   }
+
+// sayHello()
+
+
+// function Person(n){
+//   this.name=n
+// }
+
+// const new_=new Person('deepak')
+// new_
+
+//  const Person = (n) =>{
+//     this.name=n
+//   }                          #It shows person is not a constructor 
+  
+//   const new_=new Person('deepak')
+//   new_
+
+
+// Object Method
+
+// const obj={
+//   talk: function(){
+//     console.log('Deepak')
+//   }
+// }
+
+// obj.talk()
+
+
+// const obj={
+//     talk: ()=>console.log('Deepak'),    //It is working
+//   }
+  
+//   obj.talk()
+
+
+  const fun ={
+    name:'deepak',
+    sayname: ()=>{
+      return this
+    },
+    sayhello: function(){
+      return this
+    }
+
+  }
+
+  console.log(fun.sayhello())
 
